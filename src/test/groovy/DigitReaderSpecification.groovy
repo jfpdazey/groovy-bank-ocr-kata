bddf3fba98600ff1baf964a37e90a1b9ada1880c
John Dazey <jfpdazey@gmail.com>
2012-03-17 01:51:34 -0400

    Trying out parametrized test to simplify code

--- src/test/groovy/DigitReaderSpecification.groovy
+++ src/test/groovy/DigitReaderSpecification.groovy
@@ -1,45 +1,15 @@
-import spock.lang.Specification
+import spock.lang.*
 
 class DigitReaderSpecification extends Specification {
     def reader = new DigitReader()
-    
-    def "reader can read a 1"() {    
-        expect: reader.read(Digit.ONE) == 1
-    }
-    
-    def "reader can read a 2"() {     
-        expect: reader.read(Digit.TWO) == 2
-    }
-    
-    def "reader can read a 3"() {
-        expect: reader.read(Digit.THREE) == 3
-    }
-    
-    def "reader can read a 4"() {
-        expect: reader.read(Digit.FOUR) == 4
-    }
-    
-    def "reader can read a 5"() {
-        expect: reader.read(Digit.FIVE) == 5
-    }
-    
-    def "reader can read a 6"() {    
-        expect: reader.read(Digit.SIX) == 6
-    }
-    
-    def "reader can read a 7"() {     
-        expect: reader.read(Digit.SEVEN) == 7
-    }
-    
-    def "reader can read a 8"() {
-        expect: reader.read(Digit.EIGHT) == 8
-    }
-    
-    def "reader can read a 9"() {
-        expect: reader.read(Digit.NINE) == 9
-    }
-    
-    def "reader can read a 0"() {
-        expect: reader.read(Digit.ZERO) == 0
+
+    @Unroll({ "reader can read a $digit" })
+    def "reader can read each digit from 0-9"() {
+        expect: reader.read(digitText) == digit  
+        
+        where:
+            digitText << [ Digit.ZERO, Digit.ONE, Digit.TWO, Digit.THREE, Digit.FOUR,
+                            Digit.FIVE, Digit.SIX, Digit.SEVEN, Digit.EIGHT, Digit.NINE ]
+            digit << (0..9) 
     }
 }
